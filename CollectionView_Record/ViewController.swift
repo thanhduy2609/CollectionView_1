9f4b43cd4830ca68f4648ac0bb577005a0b635bf
Duy Bùi <duybui@Duys-Mac.local>
2017-04-29 13:56:23 +0700

    First init

--- CollectionView_Record/ViewController.swift
+++ CollectionView_Record/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    var images = ["one", "two", "three","one", "two", "three"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.myCollectionView.delegate = self;
+        self.myCollectionView.dataSource = self;
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! MyCollectionViewCell
+        cell.myImageViewCell?.image = UIImage(named: images[indexPath.row]);
+        return cell;
+    }
 
 }
 
